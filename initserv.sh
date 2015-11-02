b105eb7bcb9603ea7c02beae03a26e7e6740acb2
Jeanmichel Cote <webdev@jeanmichelcote.com>
2015-11-01 21:12:45 -0500

    Edited the init file

--- initserv.sh
+++ initserv.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 mydir="${HOME}/tmp"
-imported="dotfiles scripts"
-echo "$imported"
-exit 0
+
 [ ! -d "$mydir" ] && mkdir "$mydir"
 cd "$mydir"
+
 #git clone https://github.com/jeanmichelcote/serverutils.git .
 
 files="${mydir}/dotfiles/*"
+echo "$files"
+exit 0
 for f in $files; do
 	 echo "Processing $f file..."
 done
