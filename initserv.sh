85b7cab15cfdc265f8e4e0ecfcd1bd898ea912b9
Jeanmichel Cote <webdev@jeanmichelcote.com>
2015-11-02 09:48:58 -0500

    Edited the serverutils directory

--- initserv.sh
+++ initserv.sh
@@ -1,39 +1,40 @@
 #!/bin/sh
 
 nameAlreadyUsed() {
-#: Description:         Verifies if the name entered as second argument exists
-#:                      in the directory path entered as first argument
-#: Arguments:       
-#:                 $1   Path to search
-#:                 $2   Name to target
-#: a
-#: (exemple):           nameAlreadyUsed "path/to/dir/" "newname"
-#:                      nameAlreadyUsed -f funcname
+  #: Description:         Verifies if the name entered as second argument exists
+  #:                      in the directory path entered as first argument
+  #: Arguments:       
+  #:                 $1   Path to search
+  #:                 $2   Name to target
+  #: a
+  #: (exemple):           nameAlreadyUsed "path/to/dir/" "newname"
+  #:                      nameAlreadyUsed -f funcname
 
   local dest name OPTIND
 
   dest="$1"
   name="$2"
 
-  if [[ -f "${dest}/${name}" ]]; then
+  if [ -f "${dest}/${name}" ]; then
     # msg_alert "The name chosen for your new script is already used. Please choose another name."
     exit 1
   fi
 } 
 
 main() {
-mydir="${HOME}/tmp"
+  mydir="${HOME}/tmp"
 
-[ ! -d "$mydir" ] && mkdir "$mydir"
-cd "$mydir"
+  [ ! -d "$mydir" ] && mkdir "$mydir"
+  #cd "$mydir"
 
-#git clone https://github.com/jeanmichelcote/serverutils.git .
+  git clone https://github.com/jeanmichelcote/serverutils.git "$mydir"
+  
+#  files=""
+#  for f in $files; do
+#     echo "Processing $f file..."
+#  done
 
-for f in $files; do
-	 echo "Processing $f file..."
-done
-
-# cp dotfiles/* .
+  # cp dotfiles/* .
 }
 
 main
