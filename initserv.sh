d4defb70023ea1c08d024b77fc450d8d9fd6d858
Jeanmichel Cote <webdev@jeanmichelcote.com>
2015-11-02 09:43:15 -0500

    Update

--- initserv.sh
+++ initserv.sh
@@ -3,10 +3,6 @@
 nameAlreadyUsed() {
 #: Description:         Verifies if the name entered as second argument exists
 #:                      in the directory path entered as first argument
-#: Options:             
-#:                 -f   Searches for a function name.
-#:                      If this flag is up, no path should be entered as first
-#:                      argument but rather only the name of the function.
 #: Arguments:       
 #:                 $1   Path to search
 #:                 $2   Name to target
@@ -14,34 +10,18 @@ nameAlreadyUsed() {
 #: (exemple):           nameAlreadyUsed "path/to/dir/" "newname"
 #:                      nameAlreadyUsed -f funcname
 
-  local dest name nameUsed OPTIND
+  local dest name OPTIND
 
   dest="$1"
   name="$2"
 
-  [ "$#" -ne 2 ] && printusage -H >&2 && return 1
-
-  while getopts ":f" opt; do
-    case $opt in
-      f)  nameUsed=$( grep "$name" <(funx) )
-          if [[ -n "$nameUsed" ]]; then
-            msg_alert "This function name is already used. Please choose another one."
-            sleep 1.5
-            funx "$name"
-            exit 1
-          fi
-      ;;
-      *) return 1  ;;
-    esac
-  done
-  shift $(( OPTIND - 1))
-
   if [[ -f "${dest}/${name}" ]]; then
-    msg_alert "The name chosen for your new script is already used. Please choose another name."
+    # msg_alert "The name chosen for your new script is already used. Please choose another name."
     exit 1
   fi
 } 
 
+main() {
 mydir="${HOME}/tmp"
 
 [ ! -d "$mydir" ] && mkdir "$mydir"
@@ -54,3 +34,6 @@ for f in $files; do
 done
 
 # cp dotfiles/* .
+}
+
+main
